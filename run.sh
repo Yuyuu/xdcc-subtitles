8cb86f8b8c0336bf74b3a46c72f3ba94dee80fa8
Vincent Tertre <vincent.tertre@gmail.com>
2015-04-10 20:02:21 +0200

    directly send subtitles to response

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+foreman start -p 8085
--- subtitles/views.py
+++ subtitles/views.py
@@ -7,8 +7,24 @@ def index(request):
     return HttpResponse("Subtitles index.")
 
 
-def search(request):
-    languages = {Language(language) for language in request.GET.get('l').split(',')}
-    videos = {subliminal.Video.fromname(name) for name in request.GET.get('q').split(',')}
-    links = subliminal.get_best_subtitles_links(videos, languages)
-    return JsonResponse(links)
+def download(request):
+    language_query = request.GET.get('l')
+    video_query = request.GET.get('q')
+
+    if language_query is None or video_query is None:
+        response = JsonResponse({'errors': [{'message': 'MISSING_PACK_OR_LANGUAGE'}]})
+        response.status_code = 400
+        return response
+
+    language = Language(language_query)
+    video = subliminal.Video.fromname(video_query)
+
+    subtitles = subliminal.download_best_subtitles({video}, {language})
+    if len(subtitles[video]) == 0:
+        response = HttpResponse("No subtitles found for %s" % video.name)
+    else:
+        content = subtitles[video][0].content
+        response = HttpResponse(content, content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment; filename=%s.srt' % video.name
+
+    return response
